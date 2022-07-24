cde50c6e4e044d31024848b910eff710daad7b3f
Garrison Hinshaw <garrison.hinshaw18@gmail.com>
2022-07-24 12:32:43 -0400

    Added wizard as placeholder working on moving

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const beginButton = document.getElementById('beginButton')
-
-
-
-
+const scoreKeeper = document.querySelector('#score-keeper span')
+let wizard = document.getElementById('wizard')
 
 
 
@@ -14,21 +12,4 @@ beginButton.addEventListener("click", (event) => {
 function playGame(){
     beginButton.style.display = 'none'
     howToPlay.style.display = 'none'
-}
-
-function moveCharacter(){
-if(direction === 'west'){
-    x-=1
-}
-if(direction === 'east'){
-    x+=1
-}
-{
-    Element.style.left = x + 'px'
-    Element.style.bottom = y + 'px'
-}
-setInterval(moveCharacter, 1)
-document.addEventListener('keypress', function(e)=>{
-    
-})
 }
