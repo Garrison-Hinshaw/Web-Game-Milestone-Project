f68f44257229e27f8c11be59d1d53c7572438901
Garrison Hinshaw <garrison.hinshaw18@gmail.com>
2022-07-24 11:26:57 -0400

    got rid of tps file

--- index.js
+++ index.js
@@ -15,3 +15,20 @@ function playGame(){
     beginButton.style.display = 'none'
     howToPlay.style.display = 'none'
 }
+
+function moveCharacter(){
+if(direction === 'west'){
+    x-=1
+}
+if(direction === 'east'){
+    x+=1
+}
+{
+    Element.style.left = x + 'px'
+    Element.style.bottom = y + 'px'
+}
+setInterval(moveCharacter, 1)
+document.addEventListener('keypress', function(e)=>{
+    
+})
+}
