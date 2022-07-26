90ade398c0ece22aa63e8f0c58a45cf0e6e4b80a
Garrison Hinshaw <garrison.hinshaw18@gmail.com>
2022-07-25 20:59:21 -0400

    added inputhandler function

--- index.js
+++ index.js
@@ -1,4 +1,49 @@
 window.addEventListener('load', function(){
     const canvas = document.getElementById("playArea");
     const ctx = canvas.getContext('2d');
+    canvas.width = 1400;
+    canvas.height = 780;
+
+    class InputHandler {
+        constructor(){
+            this.keys = [];
+            window.addEventListener('keydown', e => {
+                if (e.key === 's' || e.key === 'a' || e.key === 'd' || e.key === ' ' || e.key === 'w' && this.keys.indexOf(e.key) === -1){
+                    this.keys.push(e.key);
+                }
+                console.log(e.key, this.keys);
+            });
+            window.addEventListener('keyup', e => {
+                if (e.key === 's' || e.key === 'a' || e.key === 'd' || e.key === ' ' || e.key === 'w'){
+                    this.keys.splice(this.keys.indexOf(e.key), 1);
+                }
+                console.log(e.key, this.keys);
+            });
+        }
+    }
+
+    class Player {
+
+    }
+
+    class Background {
+
+    }
+
+    class Enemy {
+
+    }
+
+    function handleEnemies(){
+
+    }
+
+    function displayStartText(){
+
+    }
+
+    const input = new InputHandler();
+    function animate(){
+
+    }
 })
